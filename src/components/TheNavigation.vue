e3bfb7026ef3603f61b77a3c7c8ed4b21f29867b
mayankFront <mayank.pandey@quantfarm.com>
2023-02-15 20:05:22 +0530

    Vue Router

---
+++ src/components/TheNavigation.vue
@@ -0,0 +1,30 @@
+<template>
+    <div id="nav">
+        <router-link id="logo" to="/">Abhyas Router</router-link>
+        <router-link v-for="destination in destinations" :key="destination.id"
+            :to="{ name: 'destination.show', params: { id: destination.id, slug: destination.slug } }">{{
+                destination.name
+            }}</router-link>
+    </div>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+    data () {
+        return {
+            destinations: sourceData.destinations
+        }
+    }
+}
+</script>
+
+<style lang="css">
+#nav .router-abhyas-active-class {
+    padding: 2px 3px;
+    color: #2c3e50;
+    background-color: #fff;
+    border-radius: 2px;
+}
+</style>
---
+++ src/views/DestinationShow.vue
@@ -0,0 +1,49 @@
+<template>
+    <section v-if="destination" class="destination">
+        <h1>
+            {{ destination.name }}
+        </h1>
+        <div class="destination-details">
+            <img :src="`/images/${destination.image}`" :alt="destination.name" />
+            <p>
+                {{ destination.description }}
+            </p>
+        </div>
+    </section>
+</template>
+
+<script>
+import sourceData from '@/data.json'
+
+export default {
+    data () {
+        return {
+            destination: null
+        }
+    },
+    computed: {
+        destinationId () {
+            return parseInt(this.$route.params.id);
+        },
+
+        // destination () {
+        //     return sourceData.destinations.find(destination => destination.id === this.destinationId)
+        // }
+    },
+
+    async created () {
+        this.InitData();
+
+        // this.$watch(
+        //     () => this.$route.params, this.InitData
+        // ) // Can be also done using :key="$route.path"
+    },
+
+    methods: {
+        async InitData () {
+            const response = await fetch(`https://travel-dummy-api.netlify.app/${this.$route.params.slug}.json`);
+            this.destination = await response.json();
+        }
+    }
+}
+</script>
